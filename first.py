2c16af21e9359c79be41a357a914cb0bccc29305
Happy <hardeepbharaj1295@gmail.com>
2019-06-16 18:41:25 +0530

    Create Todo list

--- first.py
+++ first.py
@@ -1,4 +1,9 @@
+# TODO : 1 first import the tkinter module
 from tkinter import *
 
+
+# TODO : 2 To create a main window , tkinter offers a method TK()
 x = Tk()
+
+# TODO : 3 mainloop() method is used when your application is ready to run
 x.mainloop()
